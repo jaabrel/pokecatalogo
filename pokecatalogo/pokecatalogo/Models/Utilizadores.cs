ea39b96c3c84e0f4234854a98df8542b5ae36f95
MegazardZ <rodrigopinheiro2005@gmail.com>
2025-04-30 10:34:36 +0100

    Branch

--- pokecatalogo/pokecatalogo/Models/Utilizadores.cs
+++ pokecatalogo/pokecatalogo/Models/Utilizadores.cs
@@ -5,5 +5,16 @@ namespace pokecatalogo.Models;
 
 public class Utilizadores
 {
+    public int Id { get; set; }
+    
+    
+    [Required(ErrorMessage = "É necessário informar o nome.")]
+    public string Nome { get; set; }
+    
+    [Required(ErrorMessage = "É necessário usar um email.")]
+    public string Email { get; set; }
+    
+    [StringLength(50)]
+    public string? IdentityUserName { get; set; }
     
 }
