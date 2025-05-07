711466fc4fb9ea67725c54445cd5779a4067a11d
João Abreu <jaabrel@outlook.pt>
2025-05-07 09:25:23 +0100

    Controller de Utilizadores

--- pokecatalogo/pokecatalogo/Models/Utilizadores.cs
+++ pokecatalogo/pokecatalogo/Models/Utilizadores.cs
@@ -5,5 +5,21 @@ namespace pokecatalogo.Models;
 
 public class Utilizadores
 {
+    /// <summary>
+    /// Identificador único do utilizador
+    /// </summary>
     public int Id { get; set; }
+    
+    /// <summary>
+    /// Nome do utilizador
+    /// </summary>
+    [Required(ErrorMessage = "O {0} é de preenchimento obrigatório.")]
+    public string Nome { get; set; }
+    
+    /// <summary>
+    /// Ligação do utilizador da base de dados criada e da tabela ASPNet Users
+    /// </summary>
+    [StringLength(50)]
+    public string? IdentityUserName { get; set; }
+    
 }
